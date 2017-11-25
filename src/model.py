Fix output no softmax and dims call in softmax

src/model.py
@@ -1,6 +1,9 @@
+import pdb
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.nn.utils.rnn import pack_padded_sequence
+from torch.nn.utils.rnn import pad_packed_sequence
 from torch.autograd import Variable
 
 class EncoderRNN(nn.Module):
@@ -20,10 +23,12 @@ class EncoderRNN(nn.Module):
         elif rnn_type == 'LSTM':
             self.rnn = nn.LSTM(hidden_size, hidden_size, n_layers)
 
-    def forward(self, input, hidden):
-        output = self.embedding(input).view(input.size(0), 1, -1)
+    def forward(self, input, hidden, input_lengths):
+        embedding = self.embedding(input)
+        output = pack_padded_sequence(embedding, input_lengths)
         for i in xrange(self.n_layers):
             output, hidden = self.rnn(output, hidden)
+        output, output_lengths = pad_packed_sequence(output)
         return output, hidden
 
     def init_hidden(self):
@@ -53,10 +58,10 @@ class DecoderRNN(nn.Module):
             self.rnn = nn.LSTM(hidden_size, hidden_size, n_layers)
 
         self.out = nn.Linear(hidden_size, output_size)
-        self.softmax = nn.LogSoftmax(dim=1)
+        # self.softmax = nn.LogSoftmax(dim=1)  # done in masked_cross_ent
 
     def forward(self, input, hidden):
-        output = self.embedding(input).view(1, 1, -1)
+        output = self.embedding(input).view(1, input.size(0), -1)
         for i in xrange(self.n_layers):
             output = F.relu(output)
             output, hidden = self.rnn(output, hidden)