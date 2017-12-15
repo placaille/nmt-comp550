19deb5fa38380bfc50cc06e283b6740245fd440f
Philippe Lacaille <philippe.lacaille@gmail.com>
2017-12-15 15:29:09 -0500

    added args for embedding pre-trained

--- launch.sh
+++ launch.sh
@@ -2,6 +2,7 @@
 data_dir=./data/multi30k
 out_dir=./out
 python_script=./src/main.py
+path_emb=./bin/pre-trained_emb_layer.bin
 
 if [ -z "$1" ]; then
 	run_name="temp_run"
@@ -10,9 +11,6 @@ else
 fi
 
 save_dir=$out_dir/$run_name/bin
-
-# change this to the path to the word2vec model (turn it on with --use_word2vec)
-path_word2vec=/data/milatmp1/lacaillp/models/word_embeddings/GoogleNews-vectors-negative300.bin.gz
 
 mkdir -p $save_dir
 string="$(date) - Running on $(hostname) - $run_name"
@@ -23,13 +21,13 @@ python $python_script \
 	--cuda \
 	--data $data_dir \
 	--save $save_dir \
-	--path_word_emb $path_word2vec \
+	--path_emb $path_emb \
 	--lang en-fr \
 	--lr_patience 1 \
 	--verbose \
 	--log-interval 20 \
-	--nhid 300 \
-	--emb_size 200 \
+	--nhid 400 \
+	--emb_size 300 \
 	--batch_size 64 \
 	--epochs 30 \
 	--lr 0.01 \
