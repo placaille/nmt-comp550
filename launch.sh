76986e549897663d5643245e287d4572f1f971ef
Philippe Lacaille <philippe.lacaille@gmail.com>
2017-12-15 12:43:25 -0500

    update launch with word2vec path

--- launch.sh
+++ launch.sh
@@ -11,6 +11,9 @@ fi
 
 save_dir=$out_dir/$run_name/bin
 
+# change this to the path to the word2vec model (turn it on with --use_word2vec)
+path_word2vec=/data/milatmp1/lacaillp/models/word_embeddings/GoogleNews-vectors-negative300.bin.gz
+
 mkdir -p $save_dir
 string="$(date) - Running on $(hostname) - $run_name"
 
@@ -20,17 +23,20 @@ python $python_script \
 	--cuda \
 	--data $data_dir \
 	--save $save_dir \
+	--path_word_emb $path_word2vec \
 	--lang en-fr \
 	--lr_patience 1 \
 	--verbose \
 	--log-interval 20 \
-	--nhid 500 \
-	--emb_size 500 \
-	--batch_size 100 \
+	--nhid 300 \
+	--emb_size 200 \
+	--batch_size 64 \
 	--epochs 30 \
 	--lr 0.01 \
+	--nlayers 2 \
 	--bidirectional \
-	--nlayers 1 \
+	--model LSTM \
 	--clip 1.0 \
 	--dropout 0.3 \
-	--teacher_force_prob 0.3
+	--teacher_force_prob 0.3 \
+	--use_word_emb
