f74cdb739db1ac48660db035f922d97cc0b5efd8
Lucas Page-Caccia <lpagec@cedar5.cedar.computecanada.ca>
2017-12-19 11:30:26 -0800

    batch launch

--- batch_launch.sh
+++ batch_launch.sh
@@ -3,20 +3,19 @@ data_dir=./data/multi30k
 out_dir=./out
 python_script=./src/main.py
 
-nlayer_choice=( 1 2 )
-emb_size_choice=( 150 300 )
-n_hid_choice=( 100 200 300 )
-tf_p_choice=( 0.3 0.5 0.7 )
-dropout_choice=( 0 0.3 0.5 0.7 )
-model_choice=( "GRU" "LSTM" )
+nlayer_choice=( 1 ) #2 )
+emb_size_choice=( 250 300 400 ) #300 )
+n_hid_choice=( 500 600 ) #200 300 )
+tf_p_choice=( 0.2 0.3 ) #0.5 0.8 )
+dropout_choice=( 0.5 ) #0.7 )
+model_choice=( "LSTM" ) #"LSTM" )
 
 
-string="$(date) - Running on $(hostname) - $run_name"
+string="$(date) - Running on $(hostname) batch"
 
 echo $string
-echo $string >> logs
 
-run_no=0
+run_no=433 # same but activate image conditioning
 for nlayer in "${nlayer_choice[@]}"
 do 
     for emb_size in "${emb_size_choice[@]}"
@@ -29,29 +28,22 @@ do
                 do 
                     for model in "${model_choice[@]}"
                     do 
+			echo $run_no
+			run_no=$((run_no+1))
                         save_dir=$out_dir/$run_no/bin
-                        mkdir -p $save_dir
-                        CUDA_VISIBLE_DEVICES=$1 python $python_script \
-                             --cuda \
-                             --data $data_dir \
-                             --save $save_dir \
-                             --lang en-de \
-                             --lr_patience 1 \
-                             --verbose \
-                             --log-interval 20 \
-                             --nhid $n_hid \
-                             --emb_size  $emb_size \
-                             --batch_size 100 \
-                             --epochs 30 \
-                             --lr 0.01 \
-                             --nlayers $nlayer \
-                             --clip 1.0 \
-                             --dropout $dropout \
-                             --use_attention \
-                             --teacher_force_prob $tf_p \
-                             --img_conditioning 1 \
-                             --model GRU
-                        run_no=$((run_no+1))
+                       	mkdir -p $save_dir
+			sbatch --gres=gpu:1 \
+			         --cpus-per-task=1 \
+			         --mem=9999M \
+			         --output=$out_dir/$run_no/log.txt \
+			         --time=0-1:39 \
+			         launch.sh $save_dir \
+			 		   $n_hid \
+			 		   $emb_size \
+			 		   $nlayer \
+					   $model \
+			 		   $dropout \
+       			 	           $tf_p 
                     done
                 done
             done
