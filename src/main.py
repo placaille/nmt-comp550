b6d749ec96835cbd20302d28e8a0697a28165639
Lucas PAGE-CACCIA <lucas.page-caccia@mail.mcgill.ca>
2017-12-19 19:59:41 -0500

    added 2 encoder support

--- src/main.py
+++ src/main.py
@@ -74,6 +74,8 @@ parser.add_argument('--lang', type=str,  default='en-fr',
                     help='in-out languages')
 parser.add_argument('--verbose', action='store_true',
                     help='verbose flag')
+parser.add_argument('--two_lang_decode', action='store_true',
+                    help='use 2 separate decoders in 2 different languages')
 args = parser.parse_args()
 
 # ensure embedding size of model is 300 for word embeddings
@@ -100,7 +102,12 @@ if torch.cuda.is_available():
 
 if args.verbose:
     print('Processing data..')
-corpus = data.Corpus(args.data, args.lang, load_img_feat=(args.img_conditioning != 0))
+
+if args.two_lang_decode:
+    corpus   = data.Corpus(args.data, 'en-fr', load_img_feat=(args.img_conditioning != 0))
+    corpus_b = data.Corpus(args.data, 'en-de', load_img_feat=(args.img_conditioning != 0))
+else: 
+    corpus = data.Corpus(args.data, args.lang, load_img_feat=(args.img_conditioning != 0))
 
 # save the dictionary for generation
 with open(os.path.join(args.save, 'vocab.pt'), 'wb') as f:
@@ -117,7 +124,10 @@ if args.verbose:
 encoder, decoder = model.build_model(len(corpus.dictionary['src']),
                                      len(corpus.dictionary['tgt']),
                                      args=args)
-
+if args.two_lang_decode: 
+    _, decoder_b = model.build_model(len(corpus_b.dictionary['src']), 
+                                     len(corpus_b.dictionary['tgt']), 
+                                     args=args)
 if args.use_word_emb:
     if args.verbose:
         print('Loading pre-trained Word2Vec embedding layer..')
@@ -135,11 +145,12 @@ if args.use_word_emb:
 if args.cuda:
     encoder.cuda()
     decoder.cuda()
+    if args.two_lang_decode: decoder_b.cuda()
 
 if args.verbose:
     print(encoder)
     print(decoder)
-
+    if args.two_lang_decode: print(decoder_b)
 
 lr = args.lr
 optimizer = torch.optim.Adam(list(encoder.parameters()) +
@@ -148,6 +159,14 @@ optimizer = torch.optim.Adam(list(encoder.parameters()) +
 
 # scheduler to reduce the lr by 4 (*0.25) if val loss doesn't decr for 2 epoch
 scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer,
+                                                       factor=0.25,
+                                                       verbose=True,
+                                                       patience=args.lr_patience)
+if args.two_lang_decode: 
+    optimizer_b = torch.optim.Adam(list(encoder.parameters()) + 
+                                   list(decoder_b.parameters()), 
+                                   lr)
+    scheduler_b = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer_b,
                                                        factor=0.25,
                                                        verbose=True,
                                                        patience=args.lr_patience)
@@ -174,30 +193,63 @@ def train_epoch():
                                             args.cuda,
                                             shuffle=True)
 
+    if args.two_lang_decode: 
+        decoder_b.train()
+        utils.set_gradient(decoder_b, True)
+        minibatches_b = utils.minibatch_generator(args.batch_size, 
+                                                  corpus_b.train, 
+                                                  args.cuda, 
+                                                  shuffle=True)
+        total_loss_b = 0
+    
     upper_bd = 10 if args.debug else float('inf')
-    for n_batch, batch in enumerate(minibatches):
-        loss, _, _ = utils.step(encoder, decoder, batch, optimizer, True, args)
-        total_loss += loss
 
-        if n_batch % args.log_interval == 0 and n_batch > 0:
-            cur_loss = total_loss / args.log_interval
-            elapsed = time.time() - start_time
-            print('| epoch {:3d} | {:5d}/{:5d} batches | lr {:02.4f} | ms/batch {:5.2f} | '
-                  'loss {:5.2f} | ppl {:8.2f}'.format(
-                  epoch, n_batch, corpus.n_sent_train // args.batch_size, lr,
-                  elapsed * 1000 / args.log_interval, cur_loss, np.exp(cur_loss)))
-            total_loss = 0
-            start_time = time.time()
+    if args.two_lang_decode:
+        for n_batch, (batch_a, batch_b) in enumerate(zip(minibatches, minibatches_b)):
+            loss, _, _ = utils.step(encoder, decoder, batch_a, optimizer, True, args)
+            total_loss += loss
 
-        if n_batch > upper_bd : break
+            loss, _, _ = utils.step(encoder, decoder_b, batch_b, optimizer_b, True, args)
+            total_loss_b += loss
 
+            if n_batch % args.log_interval == 0 and n_batch > 0:
+                cur_loss = total_loss / args.log_interval
+                cur_loss_b = total_loss_b / args.log_interval
+                elapsed = time.time() - start_time
+                print('| epoch {:3d} | {:5d}/{:5d} batches | lr {:02.4f} | ms/batch {:5.2f} | '
+                        'loss A {:5.2f} | ppl A {:8.2f} | loss B {:5.2f} | ppl B {:8.2f}'.format(
+                      epoch, n_batch, corpus.n_sent_train // args.batch_size, lr,
+                      elapsed * 1000 / args.log_interval, cur_loss, np.exp(cur_loss), 
+                      cur_loss_b, np.exp(cur_loss_b)))
+                total_loss, total_loss_b = 0, 0
+                start_time = time.time()
+
+            if n_batch > upper_bd : break
+        
+    else:
+        for n_batch, batch in enumerate(minibatches):
+            loss, _, _ = utils.step(encoder, decoder, batch, optimizer, True, args)
+            total_loss += loss
+
+            if n_batch % args.log_interval == 0 and n_batch > 0:
+                cur_loss = total_loss / args.log_interval
+                elapsed = time.time() - start_time
+                print('| epoch {:3d} | {:5d}/{:5d} batches | lr {:02.4f} | ms/batch {:5.2f} | '
+                      'loss {:5.2f} | ppl {:8.2f}'.format(
+                      epoch, n_batch, corpus.n_sent_train // args.batch_size, lr,
+                      elapsed * 1000 / args.log_interval, cur_loss, np.exp(cur_loss)))
+                total_loss = 0
+                start_time = time.time()
+
+            if n_batch > upper_bd : break
+        
 
 if args.verbose:
     print('Starting training..')
 
 # Loop over epochs.
-best_val_loss = float('inf')
-best_epoch = 0
+best_val_loss, best_val_loss_b = [float('inf')] * 2
+best_epoch, best_epoch_b = 0, 0
 # At any point you can hit Ctrl + C to break out of training early.
 for epoch in range(1, args.epochs+1):
     epoch_start_time = time.time()
@@ -220,6 +272,27 @@ for epoch in range(1, args.epochs+1):
         torch.save(decoder.state_dict(), dec_path)
 
         best_val_loss = val_loss
+    print('-' * 89)
+    
+    if args.two_lang_decode: 
+        val_loss_b, _ = utils.evaluate(corpus_b.valid, encoder, decoder_b, args, corpus=corpus)
+        scheduler_b.step(val_loss_b)
+        print('-' * 89)
+        print('| end of epoch {:3d} | time: {:5.2f}s | valid loss B {:5.2f} | '
+          'val ppl B {:5.2f}'.format(epoch, (time.time() - epoch_start_time),
+                                   val_loss_b, np.exp(val_loss_b)))
+        # Save the model if the validation loss is the best we've seen so far.
+        if val_loss_b < best_val_loss_b:
+            best_epoch_b = epoch
+            if args.verbose:
+                print('| Saving model parameters..')
+
+            enc_path = os.path.join(args.save, 'encoder_b_params.pt')
+            dec_path = os.path.join(args.save, 'decoder_b_params.pt')
+            torch.save(encoder.state_dict(), enc_path)
+            torch.save(decoder_b.state_dict(), dec_path)
+
+            best_val_loss = val_loss
     print('-' * 89)
 
 print('=' * 89)
